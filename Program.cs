a6899b15e2d213166bf324b717ac7299cddc6abd
uditha <128778821+uditha2001@users.noreply.github.com>
2024-11-23 21:29:25 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace whetherApp
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(true);
             Application.Run(new Form1());
         }
     }
